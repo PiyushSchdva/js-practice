bdd80dc1c65b8e2bc3636fe2e810b4936ad95098
Piyush Sachdeva <piyushschdva@gmail.com>
2026-06-23 13:43:57 +0530

    New commit

--- src/Work.java
+++ src/Work.java
@@ -3,6 +3,11 @@ public class Work {
         System.out.println("Work");
 
         System.out.println("Hey there");
+
+        System.out.println("Let's see if this program works");
+
+        int number = 24;
+        System.out.println("Sum is:" + number);
     }
 
 
